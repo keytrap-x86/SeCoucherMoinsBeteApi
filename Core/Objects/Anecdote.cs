e09a9cc060dda90e87db1628663357c0b1f4bd3d
Keytrap <badniiw@gmail.com>
2020-08-18 22:33:33 +0200

    Add project files.

---
+++ Core/Objects/Anecdote.cs
@@ -0,0 +1,20 @@
+﻿namespace SeCoucherMoinsBeteApi.Core.Objects
+{
+    public class Anecdote
+    {
+        public string Id { get; set; }    
+
+        public string Body { get; set; }
+
+        public Anecdote(string id, string body)
+        {
+            Id = id;
+            Body = System.Net.WebUtility.HtmlDecode(body);
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] [{Body}]";
+        }
+    }
+}
---
+++ ScmbApi.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+
+using SeCoucherMoinsBeteApi.Core.Objects;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeCoucherMoinsBeteApi
+{
+    public static class ScmbApi
+    {
+
+        public static async Task<List<Anecdote>> GetAnecdotes(string category)
+        {
+            try
+            {
+                var anecdotes = new List<Anecdote>();
+                using (var http = new HttpClient())
+                {
+                    var response = await http.GetByteArrayAsync("http://secouchermoinsbete.fr/" + category);
+                    var resp = Encoding.UTF8.GetString(response, 0, response.Length - 1);
+
+
+                    if (string.IsNullOrEmpty(resp))
+                        return null;
+
+                    var document = new HtmlDocument();
+                    document.LoadHtml(resp);
+                    var articleNodes = document.DocumentNode.SelectNodes("//div[@class='anecdote-content-wrapper']/*/a");
+                    if (articleNodes == null)
+                        return null;
+
+                    anecdotes.AddRange(from aNode in articleNodes.Nodes()
+                            .Where(n => n.InnerText != "En savoir plus")
+                        let id = aNode.ParentNode.ParentNode.ParentNode.ParentNode.Attributes["data-id"]
+                            .Value
+                        let body = aNode.InnerText.Trim()
+                        select new Anecdote(id,
+                            body));
+
+                    return anecdotes;
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+
+            return null;
+        }
+    }
+}
